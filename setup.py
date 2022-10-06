d7c73bbaf3b2554038f08231f34fc414decc882d
lorenzo angeli <hdd@users.noreply.github.com>
2022-10-06 14:31:30 +0200

    Update setup.py
    update reference to readme.rst to readme.md

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools.command.test import test as TestCommand
 
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 SOURCE_PATH = os.path.join(ROOT_PATH, 'source')
-README_PATH = os.path.join(ROOT_PATH, 'README.rst')
+README_PATH = os.path.join(ROOT_PATH, 'README.md')
 
 
 try:
